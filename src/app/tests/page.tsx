1b006218fe3ac5ebfcba1483481acc99b916274c
bluejutzu <kalebdaniel98@gmail.com>
2024-10-26 12:26:47 +0200

    Test API (success)

---
+++ src/app/tests/page.tsx
@@ -0,0 +1,12 @@
+import AxiosComponent from "@/components/main/ApiTest";
+import React from "react";
+
+const page = () => {
+    return (
+        <div>
+            <AxiosComponent />
+        </div>
+    );
+};
+
+export default page;
---
+++ src/components/main/ApiTest.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+
+const IS_DEV = process.env.NEXT_PUBLIC_ENVIRONMENT === "development";
+
+const AxiosComponent = () => {
+    const [res, setRes] = useState(null);
+
+    const handleOnClick = () => {
+        if (IS_DEV) {
+            console.log("Running in development mode");
+        } else {
+            console.log("Running in production mode");
+        }
+
+        const BASE_URL = IS_DEV ? "http://localhost:3000" : "https://denonextapp.deno.dev/";
+
+        axios
+            .get(BASE_URL + "/api")
+            .then(response => {
+                setRes(response.data)
+                console.log(response.data);
+            })
+            .catch(error => {
+                setRes(error.message)
+                console.log(error.message);
+            });
+    };
+
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <Button onClick={handleOnClick}>Send API Request</Button>
+            <div>{res ? res : "No response yet"}</div>
+        </div>
+    );
+};
+
+export default AxiosComponent;
